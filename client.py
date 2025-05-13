6dfb96bef80d96aaf55522e4bfc3bb522c33e4bc
SferrellaA <alexander@sferrella.com>
2025-05-13 08:50:23 -0400

    morniing code

--- client.py
+++ client.py
@@ -1,4 +1,5 @@
 from urllib import request
+from urllib.error import URLError
 import json
 
 endpoint="https://..."
@@ -12,3 +13,21 @@ try:
     print(response)
 except Exception as e:
     print(e)
+
+# TODO - url/header class?
+# TODO - better names; make the flow make sense
+
+def stream_request(request):
+    for chunk in response:
+        yield chunk.decode() #.strip() ?
+
+def request_endpoint(url:str):
+    try:
+        response = request.urlopen(url)
+        if response.getheader('Transfer-Encoding') == 'chunked':
+            return stream_request(request)
+        return response.read()
+    except URLError as e:
+        print(e)
+    except Exception as e:
+        print(e)
